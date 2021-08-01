17897660d446e142feea03febe43523a19536174
devrimcavusoglu <devrimcavusoglu@gmail.com>
2021-08-01 18:12:29 +0300

    Dependencies for CI corrected.

--- setup.py
+++ setup.py
@@ -27,7 +27,11 @@ _DEV_REQUIREMENTS = ["black==21.7b0", "flake8==3.9.2", "isort==5.9.2", "pytest>=
 
 _METRIC_REQUIREMENTS = ["sacrebleu", "bert_score"]
 
-extras = {"develop": _DEV_REQUIREMENTS, "metrics": _METRIC_REQUIREMENTS}
+extras = {
+    "develop": _DEV_REQUIREMENTS,
+    "metrics": _METRIC_REQUIREMENTS,
+    "ci": _DEV_REQUIREMENTS + _METRIC_REQUIREMENTS
+}
 
 
 setuptools.setup(
