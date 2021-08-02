7b539f403fb1a88c3814b837e570ec673af56204
devrimcavusoglu <devrimcavusoglu@gmail.com>
2021-08-03 01:02:38 +0300

    Fix trial for bert-score (with default en model).

--- tests/jury/test_jury.py
+++ tests/jury/test_jury.py
@@ -22,7 +22,7 @@ _CONCURRENT_TEST_METRICS = [
     Meteor(),
     Rouge(),
     # SacreBLEU(),  # Broken in concurrency, cannot find package 'sacrebleu'.
-    BERTScore(params={"model_type": "albert-base-v1"}),
+    BERTScore(),
     SQUAD(),
 ]
 _STR_TEST_METRICS = ["bleu", "meteor", "rouge", "sacrebleu", "bertscore", "squad"]
