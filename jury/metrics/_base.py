1f05c97329565340c1ee86482ada3b8b8297f3a8
devrimcavusoglu <devrimcavusoglu@gmail.com>
2021-09-18 13:12:20 +0300

    meteor.py revised to be extended from datasets.Metric.

--- jury/metrics/_base.py
+++ jury/metrics/_base.py
@@ -1,26 +1,32 @@
-from abc import ABC
-from typing import Dict, Union
+from typing import Dict, Optional
 
 import datasets
+import numpy
+from datasets.utils.logging import get_logger
+
+from jury.collator import Collator
+
+logger = get_logger(__name__)
 
 
-class Metric(ABC):
-    def __init__(self, metric_name: str, resulting_name: str = None, params: Dict = None):
-        self.metric_name = metric_name.lower()
-        self.resulting_name = resulting_name if resulting_name is not None else metric_name
-        self.params = params if params is not None else {}
+class Metric(datasets.Metric):
+    def __init__(self, resulting_name: Optional[str] = None, params: Optional[Dict] = None):
+        self.resulting_name = resulting_name if resulting_name is not None else self.name
+        self.params = params if params is not None else {"reduce_fn": "mean"}
+        super().__init__()
 
-    def compute(self, predictions, references, return_dict: bool = True, fn_multiple: bool = False):
-        predictions, references = self._preprocess(predictions, references, fn_multiple=fn_multiple)
-        metric = datasets.load_metric(self.metric_name)
-        result = metric.compute(predictions=predictions, references=references, **self.params)
-        return self._postprocess(result, return_dict=return_dict)
+    def evaluate(self, predictions, references, reduce_fn, **kwargs):
+        """Common interface for Jury evaluation metrics"""
+        raise NotImplementedError
 
-    def _preprocess(self, predictions, references, fn_multiple):
-        return predictions, references
+    def _compute(self, predictions, references, **kwargs):
+        reduce_fn_name = kwargs.get("reduce_fn", self.params["reduce_fn"])
+        reduce_fn = getattr(numpy, reduce_fn_name)
+        predictions, references = self._preprocess(predictions, references)
+        eval_params = {**self.params, **kwargs}
+        eval_params.pop("reduce_fn")
+        result = self.evaluate(predictions=predictions, references=references, reduce_fn=reduce_fn, **eval_params)
+        return result
 
-    def _postprocess(self, result, return_dict: bool) -> Union[Dict[str, float], float]:
-        score = result[self.metric_name]
-        if not return_dict:
-            return score
-        return {self.resulting_name: float(score)}
+    def _preprocess(self, predictions, references):
+        return Collator(predictions), Collator(references)
