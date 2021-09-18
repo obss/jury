73ceaf3d5719e51db5b7aeb08998a51d49f6682f
devrimcavusoglu <devrimcavusoglu@gmail.com>
2021-09-18 13:12:20 +0300

    meteor.py revised to be extended from datasets.Metric.

--- jury/definitions.py
+++ jury/definitions.py
@@ -4,11 +4,11 @@ from jury.metrics.rouge import Rouge
 from jury.metrics.sacrebleu import SacreBLEU
 
 DEFAULT_METRICS = [
-    BLEU(resulting_name="bleu_1", params={"max_order": 1}),
-    BLEU(resulting_name="bleu_2", params={"max_order": 2}),
-    BLEU(resulting_name="bleu_3", params={"max_order": 3}),
-    BLEU(resulting_name="bleu_4", params={"max_order": 4}),
+    # BLEU(resulting_name="bleu_1", params={"max_order": 1}),
+    # BLEU(resulting_name="bleu_2", params={"max_order": 2}),
+    # BLEU(resulting_name="bleu_3", params={"max_order": 3}),
+    # BLEU(resulting_name="bleu_4", params={"max_order": 4}),
     Meteor(),
-    Rouge(),
-    SacreBLEU(),
+    # Rouge(),
+    # SacreBLEU(),
 ]
--- jury/metrics/meteor.py
+++ jury/metrics/meteor.py
@@ -1,11 +1,150 @@
-from typing import Dict
+# coding=utf-8
+# Copyright 2020 Open Business Software Solutions, The HuggingFace Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" METEOR metric. Part of this file is adapted from the implementation
+of METEOR from datasets package. See
+https://github.com/huggingface/datasets/blob/master/metrics/meteor/meteor.py
+"""
+from typing import Dict, Optional
 
+import numpy as np
+from nltk.translate import meteor_score
+
+import datasets
+
+from jury.collator import Collator
 from jury.metrics import Metric
 
 __class_names__ = {"meteor": "Meteor"}
 
+_CITATION = """\
+@inproceedings{banarjee2005,
+  title     = {{METEOR}: An Automatic Metric for {MT} Evaluation with Improved Correlation with Human Judgments},
+  author    = {Banerjee, Satanjeev  and Lavie, Alon},
+  booktitle = {Proceedings of the {ACL} Workshop on Intrinsic and Extrinsic Evaluation Measures for Machine Translation and/or Summarization},
+  month     = jun,
+  year      = {2005},
+  address   = {Ann Arbor, Michigan},
+  publisher = {Association for Computational Linguistics},
+  url       = {https://www.aclweb.org/anthology/W05-0909},
+  pages     = {65--72},
+}
+"""
 
+_DESCRIPTION = """\
+METEOR, an automatic metric for machine translation evaluation
+that is based on a generalized concept of unigram matching between the
+machine-produced translation and human-produced reference translations.
+Unigrams can be matched based on their surface forms, stemmed forms,
+and meanings; furthermore, METEOR can be easily extended to include more
+advanced matching strategies. Once all generalized unigram matches
+between the two strings have been found, METEOR computes a score for
+this matching using a combination of unigram-precision, unigram-recall, and
+a measure of fragmentation that is designed to directly capture how
+well-ordered the matched words in the machine translation are in relation
+to the reference.
+
+METEOR gets an R correlation value of 0.347 with human evaluation on the Arabic
+data and 0.331 on the Chinese data. This is shown to be an improvement on
+using simply unigram-precision, unigram-recall and their harmonic F1
+combination.
+"""
+
+_KWARGS_DESCRIPTION = """
+Computes METEOR score of translated segments against one or more references.
+Args:
+    predictions: list of predictions to score. Each prediction
+        should be a string with tokens separated by spaces.
+    references: list of reference for each prediction. Each
+        reference should be a string with tokens separated by spaces.
+    alpha: Parameter for controlling relative weights of precision and recall. default: 0.9
+    beta: Parameter for controlling shape of penalty as a function of fragmentation. default: 3
+    gamma: Relative weight assigned to fragmentation penalty. default: 0.5
+Returns:
+    'meteor': meteor score.
+Examples:
+
+    >>> meteor = datasets.load_metric('meteor')
+    >>> predictions = ["It is a guide to action which ensures that the military always obeys the commands of the party"]
+    >>> references = ["It is a guide to action that ensures that the military will forever heed Party commands"]
+    >>> results = meteor.compute(predictions=predictions, references=references)
+    >>> print(round(results["meteor"], 4))
+    0.7398
+"""
+
+
+@datasets.utils.file_utils.add_start_docstrings(_DESCRIPTION, _KWARGS_DESCRIPTION)
 class Meteor(Metric):
-    def __init__(self, metric_name: str = None, resulting_name: str = None, params: Dict = None):
-        metric_name = self.__class__.__name__ if metric_name is None else metric_name
-        super().__init__(metric_name=metric_name, resulting_name=resulting_name, params=params)
+    def __init__(self, resulting_name: str = None, params: Optional[Dict] = None):
+        resulting_name = "Meteor" if resulting_name is None else resulting_name
+        super(Meteor, self).__init__(resulting_name=resulting_name, params=params)
+
+    def _info(self):
+        return datasets.MetricInfo(
+            description=_DESCRIPTION,
+            citation=_CITATION,
+            inputs_description=_KWARGS_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "predictions": datasets.Sequence(datasets.Value("string", id="sequence")),
+                    "references": datasets.Sequence(datasets.Value("string", id="sequence")),
+                }
+            ),
+            codebase_urls=["https://github.com/nltk/nltk/blob/develop/nltk/translate/meteor_score.py"],
+            reference_urls=[
+                "https://www.nltk.org/api/nltk.translate.html#module-nltk.translate.meteor_score",
+                "https://en.wikipedia.org/wiki/METEOR",
+            ],
+        )
+
+    def _download_and_prepare(self, dl_manager):
+        import nltk
+
+        nltk.download("wordnet")
+
+    def evaluate(self, predictions: Collator, references: Collator, reduce_fn, alpha=0.9, beta=3, gamma=0.5):
+        if predictions.can_collapse() and references.can_collapse():
+            scores = [
+                meteor_score.single_meteor_score(ref, pred, alpha=alpha, beta=beta, gamma=gamma)
+                for ref, pred in zip(references, predictions)
+            ]
+        elif predictions.can_collapse() and not references.can_collapse():
+            scores = [
+                meteor_score.meteor_score(hypothesis=pred, references=ref,
+                                          alpha=alpha,
+                                          beta=beta,
+                                          gamma=gamma)
+                for ref, pred in zip(references, predictions)
+            ]
+        else:
+            scores = []
+            for pred, ref in zip(predictions, references):
+                pred_scores = []
+                for p in pred:
+                    score_p = [
+                        meteor_score.meteor_score(ref, pred, alpha=alpha, beta=beta, gamma=gamma)
+                        for ref, pred in zip(references, predictions)
+                    ]
+                    pred_scores.append(score_p)
+                scores.append(reduce_fn(pred_scores))
+
+        return {self.resulting_name: np.mean(scores)}
+
+
+if __name__ == "__main__":
+    predictions = [["It is a guide to action which ensures that the military always obeys the commands of the party"]]
+    references = [["It is a guide to action that ensures that the military will forever heed Party commands"]]
+    meteor = Meteor()
+    score = meteor.compute(predictions=predictions, references=references)
+    print(score)
