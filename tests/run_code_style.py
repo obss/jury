99403c74d37eed4e31732d7dc8d8c3bf30e14e72
devrimcavusoglu <devrimcavusoglu@gmail.com>
2021-09-20 12:47:15 +0300

    Test cases revised, assertion on test scripts are changed to system exit.

--- tests/run_code_style.py
+++ tests/run_code_style.py
@@ -1,14 +1,16 @@
 import sys
 
-from tests.utils import assert_shell
+from tests.utils import shell, validate_and_exit
 
 if __name__ == "__main__":
     arg = sys.argv[1]
 
     if arg == "check":
-        assert_shell("flake8 jury tests --config setup.cfg")
-        assert_shell("isort . --check --settings setup.cfg")
-        assert_shell("black . --check --config pyproject.toml")
+        sts_flake = shell("flake8 trapper tests --config setup.cfg")
+        sts_isort = shell("isort . --check --settings setup.cfg")
+        sts_black = shell("black . --check --config pyproject.toml")
+        validate_and_exit(sts_flake, sts_isort, sts_black)
     elif arg == "format":
-        assert_shell("isort . --settings setup.cfg")
-        assert_shell("black . --config pyproject.toml")
+        sts_flake = shell("isort . --settings setup.cfg")
+        sts_black = shell("black . --config pyproject.toml")
+        validate_and_exit(sts_flake, sts_black)
