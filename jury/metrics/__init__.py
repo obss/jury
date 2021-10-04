ecf9e0dbce1748f5763e461fb88f344a019d9db3
devrimcavusoglu <devrimcavusoglu@gmail.com>
2021-10-04 14:13:08 +0300

    __init__.py updated for metrics modules that require extra package.

--- jury/metrics/__init__.py
+++ jury/metrics/__init__.py
@@ -1,13 +1,21 @@
 from jury.metrics._base import Metric, MetricCollator, load_metric
 from jury.metrics.accuracy import Accuracy
-from jury.metrics.bertscore import Bertscore
+
+try:
+    from jury.metrics.bertscore import Bertscore
+except ModuleNotFoundError:
+    pass
 from jury.metrics.bleu import Bleu
 from jury.metrics.f1 import F1
 from jury.metrics.meteor import Meteor
 from jury.metrics.precision import Precision
 from jury.metrics.recall import Recall
 from jury.metrics.rouge import Rouge
-from jury.metrics.sacrebleu import Sacrebleu
+
+try:
+    from jury.metrics.sacrebleu import Sacrebleu
+except ModuleNotFoundError:
+    pass
 from jury.metrics.squad import Squad
 
 __all__ = [
