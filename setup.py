edd6a8a143f3856ab9561c8b5e2ac724d4f04ea3
devrimcavusoglu <devrimcavusoglu@gmail.com>
2022-02-15 18:07:50 +0300

    Code formatting.

--- setup.py
+++ setup.py
@@ -47,8 +47,13 @@ _DEV_REQUIREMENTS = [
 
 _PRISM_REQUIREMENTS = ["fairseq==0.9.0", "validators"]
 
-_METRIC_REQUIREMENTS = ["sacrebleu>=2.0.0", "bert_score==0.3.11", "seqeval==1.2.2", "sentencepiece==0.1.96",
-                        "bleurt @ git+https://github.com/google-research/bleurt.git",]
+_METRIC_REQUIREMENTS = [
+    "sacrebleu>=2.0.0",
+    "bert_score==0.3.11",
+    "seqeval==1.2.2",
+    "sentencepiece==0.1.96",
+    "bleurt @ git+https://github.com/google-research/bleurt.git",
+]
 _METRIC_REQUIREMENTS.extend(_PRISM_REQUIREMENTS)
 add_pywin(_METRIC_REQUIREMENTS)
 
