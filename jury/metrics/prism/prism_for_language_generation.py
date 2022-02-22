2a226f334fbfdd2fa7d54035bc464cd6c3a7a38a
devrimcavusoglu <devrimcavusoglu@gmail.com>
2022-02-22 18:31:31 +0300

    License information added for prism and bleurt.

--- jury/metrics/prism/prism_for_language_generation.py
+++ jury/metrics/prism/prism_for_language_generation.py
@@ -95,6 +95,32 @@ Examples:
     }
 """
 
+_LICENSE = """MIT License
+
+Copyright (c) Brian Thompson
+
+Portions of this software are copied from fairseq (https://github.com/pytorch/fairseq),
+which is released under the MIT License and Copyright (c) Facebook, Inc. and its affiliates.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 CHECKPOINT_URLS = {
     "default": {
         "url": "https://huggingface.co/Devrim/prism-default/resolve/main/m39v1.tar",
@@ -172,6 +198,7 @@ class PrismForLanguageGeneration(MetricForLanguageGeneration):
                 "https://github.com/thompsonb/prism",
                 "https://www.aclweb.org/anthology/2020.emnlp-main.8/",
             ],
+            license=_LICENSE,
         )
 
     def _load_scorer(self):
