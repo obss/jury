603b3c6d4590226dfd6d0c65c4f1a33b40e198e6
devrimcavusoglu <devrimcavusoglu@gmail.com>
2022-02-22 17:53:24 +0300

    Test cases refactored. - Bertscore removed from regular tests.

--- tests/jury/metrics/test_bartscore.py
+++ tests/jury/metrics/test_bartscore.py
@@ -7,13 +7,13 @@ from tests.utils import assert_almost_equal_dict
 
 
 @pytest.fixture(scope="module")
-def jury():
+def jury_bartscore():
     metric = AutoMetric.load("bartscore", device="cpu")
     return Jury(metrics=metric)
 
 
 @pytest.fixture(scope="module")
-def jury_segment():
+def jury_bartscore_segment():
     metric = AutoMetric.load("bartscore", device="cpu", compute_kwargs={"segment_scores": True})
     return Jury(metrics=metric)
 
@@ -42,21 +42,23 @@ def output_multiple_pred_multiple_ref():
     return output_multiple_pred_multiple_ref.output
 
 
-def test_basic(predictions, references, jury, output_basic):
-    scores = jury(predictions=predictions, references=references)
+def test_basic(predictions, references, jury_bartscore, output_basic):
+    scores = jury_bartscore(predictions=predictions, references=references)
     assert_almost_equal_dict(actual=scores, desired=output_basic)
 
 
-def test_basic_segment(predictions, references, jury_segment, output_basic_segment):
-    scores = jury_segment(predictions=predictions, references=references)
+def test_basic_segment(predictions, references, jury_bartscore_segment, output_basic_segment):
+    scores = jury_bartscore_segment(predictions=predictions, references=references)
     assert_almost_equal_dict(actual=scores, desired=output_basic_segment)
 
 
-def test_multiple_ref(predictions, multiple_references, jury, output_multiple_ref):
-    scores = jury(predictions=predictions, references=multiple_references)
+def test_multiple_ref(predictions, multiple_references, jury_bartscore, output_multiple_ref):
+    scores = jury_bartscore(predictions=predictions, references=multiple_references)
     assert_almost_equal_dict(actual=scores, desired=output_multiple_ref)
 
 
-def test_multiple_pred_multiple_ref(multiple_predictions, multiple_references, jury, output_multiple_pred_multiple_ref):
-    scores = jury(predictions=multiple_predictions, references=multiple_references)
+def test_multiple_pred_multiple_ref(
+    multiple_predictions, multiple_references, jury_bartscore, output_multiple_pred_multiple_ref
+):
+    scores = jury_bartscore(predictions=multiple_predictions, references=multiple_references)
     assert_almost_equal_dict(actual=scores, desired=output_multiple_pred_multiple_ref)
