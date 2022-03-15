7d2d39348638ac1abeb2bb545ca3c0b377aa8a41
devrimcavusoglu <devrimcavusoglu@gmail.com>
2022-03-15 11:35:00 +0300

    Setup.py updated for unbabel-comet (from spesific commit addressing fix for num_workers).

--- tests/jury/metrics/test_comet.py
+++ tests/jury/metrics/test_comet.py
@@ -9,7 +9,7 @@ from tests.utils import assert_almost_equal_dict
 @pytest.fixture(scope="module")
 def jury_comet():
     metric = AutoMetric.load(
-        "comet", compute_kwargs={"gpus": 0, "num_workers": 4, "progress_bar": False, "batch_size": 2}
+        "comet", compute_kwargs={"gpus": 0, "num_workers": 0, "progress_bar": False, "batch_size": 2}
     )
     return Jury(metrics=metric)
 
