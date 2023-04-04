ed382bc67ba61f9d21f13ccee8eab1875e672750
Nish <nishanpantha@gmail.com>
2023-04-04 17:58:54 -0500

    Add try/catch block across ZeroDivisionError for AccuracyForLanguageGeneration._compute_single_pred_single_ref (#123)
    * Add try/catch block across ZeroDivisionError
    
    * Add more verbose warning log for zero division error
    
    * Use Counter(...) intersection property to compute common values for
    pred/ref
    
    * Fix linting issue
    
    * Fix linting issue

--- jury/metrics/accuracy/accuracy_for_language_generation.py
+++ jury/metrics/accuracy/accuracy_for_language_generation.py
@@ -23,10 +23,13 @@ from typing import Callable
 
 import evaluate
 import numpy as np
+from evaluate.utils.logging import get_logger
 
 from jury.collator import Collator
 from jury.metrics._core import MetricForLanguageGeneration
 from jury.utils.nlp import normalize_text
+
+logger = get_logger(__name__)
 
 _CITATION = """\
 @article{scikit-learn,
@@ -66,7 +69,7 @@ Examples:
     >>> accuracy = jury.load_metric("accuracy")
     >>> predictions = [["the cat is on the mat", "There is cat playing on the mat"], ["Look! a wonderful day."]]
     >>> references = [
-        ["the cat is playing on the mat.", "The cat plays on the mat."], 
+        ["the cat is playing on the mat.", "The cat plays on the mat."],
         ["Today is a wonderful day", "The weather outside is wonderful."]
     ]
     >>> results = accuracy.compute(predictions=predictions, references=references)
@@ -98,13 +101,13 @@ class AccuracyForLanguageGeneration(MetricForLanguageGeneration):
         scores = []
         predictions, references = self._tokenize(predictions, references)
         for pred, ref in zip(predictions, references):
-            score = 0
-            pred_counts = Counter(pred)
-            ref_counts = Counter(ref)
-            for token, pred_count in pred_counts.items():
-                if token in ref_counts:
-                    score += min(pred_count, ref_counts[token])  # Intersection count
-            scores.append(score / max(len(pred), len(ref)))
+            common = Counter(pred) & Counter(ref)  # Intersection count
+            score = sum(common.values())
+            try:
+                scores.append(score / max(len(pred), len(ref)))
+            except ZeroDivisionError:
+                logger.warning("Empty pred & ref after preprocess (`normalize_text`) step. Ignoring!")
+
         avg_score = sum(scores) / len(scores)
         return {"score": avg_score}
 
